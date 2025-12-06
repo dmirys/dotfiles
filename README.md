f1b99d01e84584d0bfe6aebe55f70c759d4a20d0
Dmitry Yastrebkov <yastrebkov@gmail.com>
2025-12-06 20:22:55 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# dotfiles
+
+Setup of command line tools and configs (dot files) for these tools.
+Useful for fast deploy on clean Ubuntu installation.
+
+## Install and update
+
+```bash
+wget -qO- https://raw.githubusercontent.com/dmirys/dotfiles/refs/heads/master/install.sh | bash
+```
+
+## Uninstall
+
+```bash
+wget -qO- https://raw.githubusercontent.com/dmirys/dotfiles/refs/heads/master/install.sh | bash -s - -u
+```
---
+++ install.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+set -e
+
+# Installation settings:
+REPO_URL="git@github.com:dmirys/dotfiles.git"
+DOTFILES_DIR="$HOME/.dotfiles"
+BASH_CUSTOM="$HOME/.bash_custom"
+BASHRC="$HOME/.bashrc"
+BASHRC_SOURCE_LINE="source ~/.bash_custom"
+
+if [ "$1" == "-u" ] || [ "$1" == "--uninstall" ]; then
+	if grep -Fxq "$BASHRC_SOURCE_LINE" "$BASHRC"; then
+		echo "--> Removing custom config source from .bashrc..."
+		sed -i "\|$BASHRC_SOURCE_LINE|d" "$BASHRC"
+	fi
+    
+	if [ -d "$DOTFILES_DIR" ]; then
+		echo "--> Removing files tracked by dotfiles repository..."
+		FILES=$(/usr/bin/git --git-dir="$DOTFILES_DIR" --work-tree="$HOME" ls-files)
+		for file in $FILES; do
+			rm -f "$HOME/$file"
+		done
+
+		echo "--> Removing dotfiles repository..."
+		rm -rf "$DOTFILES_DIR"
+	fi
+
+	echo "[DONE] Uninstallation complete."
+	exit 0
+fi
+
+
+echo "--> Install dependencies..."
+sudo apt update
+sudo apt install -y git mc vim
+
+
+if [ -d "$DOTFILES_DIR" ]; then
+	echo "--> Updating bare-repository..."
+	git pull
+else
+	echo "--> Clonning bare-repository..."
+	git clone --bare "$REPO_URL" "$DOTFILES_DIR"
+
+	git --git-dir="$DOTFILES_DIR" --work-tree="$HOME" config core.sparseCheckout true
+	SPARSE_FILE="$DOTFILES_DIR/info/sparse-checkout"
+	mkdir -p "$(dirname "$SPARSE_FILE")"
+	cat <<-EOF | sed 's/^[[:space:]]\{1,\}//' > "$SPARSE_FILE"
+		*
+		!install.sh
+		!README.md
+	EOF
+
+	echo "--> Hiding untracked files..."
+	git --git-dir="$DOTFILES_DIR" --work-tree="$HOME" config --local status.showUntrackedFiles no
+
+	echo "--> Checking out dotfiles into \$HOME..."
+	git --git-dir="$DOTFILES_DIR" --work-tree="$HOME" checkout
+fi
+
+
+if ! grep -Fxq "$BASHRC_SOURCE_LINE" "$BASHRC"; then
+    echo "--> Adding custom config source to .bashrc..."
+    echo "$BASHRC_SOURCE_LINE" >> "$BASHRC"
+fi
+
+echo "[Done] Installation complete."
